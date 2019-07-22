f9a8a94db1aaf1669ed9e840dd9ca2827a1bcb6d
Keshav-y <51353337+Keshav-y@users.noreply.github.com>
2019-07-22 18:35:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
-$servername = "applicationtest.database.windows.net";
-$username = "DemoTest";
-$password = "Test";
-$dbname = "ApplicationTest";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "INSERT INTO [dbo].test (Name)
-VALUES ('Name')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();
+Welcome <?php echo $_POST["name"]
 ?>
